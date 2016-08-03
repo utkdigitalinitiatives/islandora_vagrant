5ce859501223064ad75eadbb940fb54bb53b5914
bridger <bdysonsm@utk.edu>
2016-08-03 16:55:18 -0400

    fix source command's file pathing

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -2,4 +2,10 @@
 
 echo "Running custom scripts"
 
-source ./custom_scripts/update_basic_solr_config.sh
+SHARED_DIR=$1
+
+if [ -f "SHARED_DIR/configs/variables" ]; then
+	. "$SHARED_DIR"/configs/variables
+fi
+
+source "$SHARED_DIR"/scripts/custom_scripts/update_basic_solr_config.sh
