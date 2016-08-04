eb6e7fb5889158d18aef671bd7e3abe5dcbed49d
bridger <bdysonsm@utk.edu>
2016-08-04 13:53:31 -0400

    add a check for the local basic-solr-config repository

--- scripts/custom_scripts/update_basic_solr_config.sh
+++ scripts/custom_scripts/update_basic_solr_config.sh
@@ -3,7 +3,12 @@
 FGS_TARGET="/var/lib/tomcat7/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/"
 
 echo "Cloning basic-solr-config"
-git clone -b 4.x --single-branch https://github.com/utkdigitalinitiatives/basic-solr-config/ 4-x-utk
+if [ ! -d "$HOME_DIR"/4-x-utk ]; then
+	git clone -b 4.x --single-branch https://github.com/utkdigitalinitiatives/basic-solr-config/ 4-x-utk
+	chown -hR vagrant:vagrant 4-x-utk
+else
+	git -C "$HOME_DIR"/4-x-utk pull
+fi
 
 echo "Updating Fedora GSearch"
 # fix file paths in the new files
@@ -17,11 +22,11 @@ sudo cp 4-x-utk/foxmlToSolr.xslt $FGS_TARGET
 sudo cp -a 4-x-utk/islandora_transforms $FGS_TARGET
 
 # update file/directory ownership
-sudo chown -R tomcat7:tomcat7 $FGS_TARGET
+sudo chown -hR tomcat7:tomcat7 $FGS_TARGET
 
 echo "Updating Solr"
 # copy the appropriate Solr config files to Solr
 sudo cp 4-x-utk/solr-conf/* /usr/local/solr/collection1/conf/
 
 # update file/directory ownership
-sudo chown -R tomcat7:tomcat7 /usr/local/solr/collection1/conf
+sudo chown -hR tomcat7:tomcat7 /usr/local/solr/collection1/conf
