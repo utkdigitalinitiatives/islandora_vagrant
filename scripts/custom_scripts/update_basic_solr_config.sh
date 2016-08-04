3200ef2b347a6956dcc4534e46eb62b69efeb70e
bridger <bdysonsm@utk.edu>
2016-08-04 13:25:46 -0400

    add asterisk to solr-conf copy command

--- scripts/custom_scripts/update_basic_solr_config.sh
+++ scripts/custom_scripts/update_basic_solr_config.sh
@@ -10,15 +10,18 @@ echo "Updating Fedora GSearch"
 sed -i 's|/vhosts/fedora/solr|/usr/local/solr|g' 4-x-utk/index.properties
 sed -i 's|/vhosts/fedora/tomcat|/var/lib/tomcat7|g' 4-x-utk/foxmlToSolr.xslt
 sed -i 's|/vhosts/fedora/tomcat|/var/lib/tomcat7|g' 4-x-utk/islandora_transforms/*.xslt
+
 # copy the appropriate fedoragsearch files to fedoragsearch
 sudo cp 4-x-utk/index.properties $FGS_TARGET
 sudo cp 4-x-utk/foxmlToSolr.xslt $FGS_TARGET
 sudo cp -a 4-x-utk/islandora_transforms $FGS_TARGET
+
 # update file/directory ownership
 sudo chown -R tomcat7:tomcat7 $FGS_TARGET
 
 echo "Updating Solr"
 # copy the appropriate Solr config files to Solr
-sudo cp 4-x-utk/solr-conf/ /usr/local/solr/collection1/conf/
+sudo cp 4-x-utk/solr-conf/* /usr/local/solr/collection1/conf/
+
 # update file/directory ownership
 sudo chown -R tomcat7:tomcat7 /usr/local/solr/collection1/conf
